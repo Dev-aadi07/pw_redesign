f87534f96449de329bf05c546204b0c9e682e10d
Adarsh <aadarshjha07137@gmail.com>
2026-05-31 13:10:36 +0530

    feat: add founder section and image hover sound effect

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ document.querySelector("#logo").addEventListener('click', function(){
     window.location.href = url;
   }
 });
+
+
+const hoverSound = document.querySelector("#hoverSound");
+const imageContainer = document.querySelector(".img-container");
+
+document.body.addEventListener("click", () => {
+  hoverSound.play();
+  hoverSound.pause();
+  hoverSound.currentTime = 0;
+}, { once: true });
+
+imageContainer.addEventListener("mouseenter", () => {
+  hoverSound.currentTime = 0;
+  hoverSound.play();
+});
+
+imageContainer.addEventListener("mouseleave", () => {
+  hoverSound.pause();
+  hoverSound.currentTime = 0;
+});
